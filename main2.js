532d965381c30c9aa28d357d947a5658dfa8cf17
YousefAlsayed4 <yalsayed19@gmail.com>
2024-01-09 12:04:37 +0200

    style: change folder structure

--- main2.js
+++
@@ -1,7 +0,0 @@
-window.alert("Show a message to tell the user that the first release of a calculator that only has a summation feature. ")
-
-var Fnum= window.prompt("Enter the first Number");
-var Snum= window.prompt("Enter the second Number");
-var sum = Number(Fnum)+Number(Snum);
-
-window.alert(`Result ${sum}`);
